70d69ecf0580b30e23bebf3954e536f3a7ecc459
geunsu.kim <geunsu.kim@navercorp.com>
2024-03-28 14:33:25 +0900

    fix: Change SPM platform versions

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(name: "ObjectMapper",
-                      platforms: [.macOS(.v10_10),
+                      platforms: [.macOS(.v10_13),
                                   .iOS(.v13),
-                                  .tvOS(.v9),
-                                  .watchOS(.v2)],
+                                  .tvOS(.v12),
+                                  .watchOS(.v4)],
                       products: [.library(name: "ObjectMapper",
                                           targets: ["ObjectMapper"])],
                       targets: [.target(name: "ObjectMapper",
